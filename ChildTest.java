57fdcef928e1625667872b0b93bae056935cb0f5
whitepaper907 <50574878+whitepaper907@users.noreply.github.com>
2023-10-26 15:02:27 +0900

    Add files via upload

---
+++ ChildTest.java
@@ -0,0 +1,28 @@
+public class ChildTest {
+    public static void print(Parent2 p){
+        System.out.println(p.i);
+        System.out.println(p.get());
+    }
+
+    public static void main(String[] args){
+        Parent2 p = new Parent2();
+        System.out.println("---------------1-----------------");
+        System.out.println(p.i);
+        System.out.println(p.get());
+
+        Child2 c = new Child2();
+        System.out.println("---------------2-----------------");
+        System.out.println(c.i);
+        System.out.println(c.get());
+
+        Parent2 p2 = new Child2();
+        System.out.println("---------------3-----------------");
+        System.out.println(p2.i);
+        System.out.println(p2.get());
+
+        System.out.println("---------------4-----------------");
+        print(c);
+        print(p2);
+
+    }
+}
